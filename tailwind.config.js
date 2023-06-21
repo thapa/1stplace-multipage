3a94d8b0bf9fc8087c216ba6f85ce4eb1ee1c7bf
thapa <thapa.shammi@gmail.com>
2023-06-21 20:54:20 +0530

    style-guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,39 +5,60 @@ module.exports = {
     ],
     theme: {
         fontFamily: {
-            sans: "proxima-nova, sans-serif"
+            sans: 'noto-sans, sans-serif'
         },
         extend: {
             container: {
                 padding: '20px'
             },
             colors: {
-                'accent': '#FFDB00',
-                'accent-2': '#1AA800',
-                'accent-3': '#315147',
-                'button': "#315147",
-                'media': "#F2F2F2"
+                // Brand Colors 
+                'brand-accent': '#E6001B',
+                'brand-dark': '#070807',
+                'brand-d-grey': '#3F3F3F',
+                'brand-m-grey': '#8E8E8E',
+                'brand-l-grey': '#D6D6D6',
+                
+                // School Colors
+
+                'school-1': '#315147',
+                'school-2': '#FFDB00',
+                'school-3': '#778E87',
+
+                // Global Colors
+
+                'global-1': '#1AA800',
+                'global-2': '#E6001B',
+                'global-l-grey': '#F2F2F2',
+                'global-dark': '#070807'
+
             },
-            fontFamily: {
-                'Proxima': "proxima-nova, sans-serif",
-                'Noto': "'Noto Sans', sans-serif",
-                'Orbitron': "'Orbitron', sans-serif",
-                'Proxima-condensed': 'proxima-nova-condensed, sans-serif;'
+            fontFamily: {                
+                'Noto': '"noto-sans", sans-serif',
+                'Noto-condensed': '"noto-sans-condensed", sans-serif',
+                'Noto-extra-condensed': '"noto-sans-extracondensed", sans-serif'
+                
             },
             maxWidth: {
-                'narrow': '1300px'
+                'main': '1460px',
+                'narrow': '1160px'
             },
             backgroundImage: {
                 'checkmark': "url('/images/check-icon.svg')",
                 'checkmark-black': "url('/images/check-icon-black.svg')"
             },
             fontSize: {
-                xxs: '12px',
-                xs: '16px',
-                sm: '20px',
-                md: '30px',
-                lg: '40px',
-                xl: '50px'
+                'size-1': '76px',
+                'size-2': '64px',
+                'size-3': '52px',
+                'size-4': '48px',
+                'size-5': '30px',
+                'size-6': '24px',
+                    base: '21px',
+                    sm: '18px',
+                    xs: '14px',
+                    xxs: '12px',
+                
             }
         },
     },
